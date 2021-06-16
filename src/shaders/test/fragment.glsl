d49552c03aca88937635c77cd7bb12ca61aed4d6
Brian Kim <brian.kyounghoon.kim@gmail.com>
2021-06-16 12:42:28 +0900

    basic pattern work with uvs

--- src/shaders/test/fragment.glsl
+++ src/shaders/test/fragment.glsl
@@ -1,4 +1,15 @@
+varying vec2 vUv;
+
 void main()
 {
-    gl_FragColor = vec4(0.5, 0.0, 1.0, 1.0);
+    float strengthY = vUv.y;
+
+    gl_FragColor = vec4(strengthY, strengthY, strengthY, 1.0);
+
+    // float strengthX = vUv.x;
+
+    // gl_FragColor = vec4(strengthX, strengthX, strengthX, 1.0);
+    
+    // gl_FragColor = vec4(vUv, 0.0, 1.0);
+    // gl_FragColor = vec4(0.5, 0.0, 1.0, 1.0);
 }
